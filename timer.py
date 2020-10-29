7fbb3a2b1ba749abb5b837bbbd420d41b91584a5
Timeli <59762421+Timeli@users.noreply.github.com>
2020-10-29 16:00:15 +0300

    Update timer.py

--- timer.py
+++ timer.py
@@ -4,21 +4,21 @@ import pygame
 
 pygame.init()
 
-# Настройки окна
+# set window
 width = 300
 height = 150
 R_scr, G_scr, B_scr = 240, 228, 209
 
-# Настройки цифр
+# set digits
 R, G, B = 162, 0, 0
 size_digits = 4
 shift = 0
 
-# Настройка кнопок
+# set button
 R_but, G_but, B_but = 209, 160, 82
 R_press, G_press, B_press = 240, 228, 209
 coord_x, coord_y = -1, -1
-# цвет иконки кнопок
+# color icon button
 R_emb, G_emb, B_emb = 162, 0, 0
 
 start = False
@@ -62,7 +62,7 @@ def number_display(index):
 
 while True:
 
-    clock.tick(30)
+    clock.tick(30)   # FPS
     screen.fill((R_scr, G_scr, B_scr))
     sec = pygame.time.get_ticks() // 1000
 
@@ -84,37 +84,37 @@ while True:
         sec -= tmp
         number_display(sec)
 
-    # кнопка старт
+    # Next need to change
+
+    # start button
     pygame.draw.lines(screen, (R_but, G_but, B_but), True, [(41, 101), (130, 101), (130, 135), (41, 135)], 2)
     pygame.draw.rect(screen, (235, 215, 183), [40, 100, 90, 35])
 
-    # нажатие на кнопку старт
+    # pressing the start button
     if 40 <= coord_x <= 120 and 100 <= coord_y <= 135 and left == 1:
         left = 0
         pygame.draw.lines(screen, (R_press, G_press, B_press), True, [(40, 100), (130, 100), (130, 135), (40, 135)], 2)
 
-    # эмблема кнопки старт
-    # pygame.draw.polygon(screen, (R_emb, G_emb, B_emb), [[78, 105], [100, 117], [78, 130]], 2)
+    # icon start button
     pygame.draw.aalines(screen, (R_emb, G_emb, B_emb), True, [[78, 105], [100, 117], [78, 130]])
 
-    # кнопка стоп
+    # stop button
     pygame.draw.lines(screen, (R_but, G_but, B_but), True, [(171, 101), (260, 101), (260, 135), (171, 135)], 2)
     pygame.draw.rect(screen, (235, 215, 183), [170, 100, 90, 35])
 
-    # нажатие на кнопку стоп
+    # pressing the stop button
     if 180 <= coord_x <= 260 and 100 <= coord_y <= 135 and right == 1:
         right = 0
         pygame.draw.lines(screen, (R_press, G_press, B_press), True, [(170, 100), (260, 100), (260, 135), (170, 135)],
                           2)
-
-    # эмблема кнопки стоп
+    # icon stop button
     pygame.draw.aalines(screen, (R_emb, G_emb, B_emb), True, [[205, 106], [228, 106], [228, 129], [205, 129]])
 
-    # центральные точки
+    # two points
     pygame.draw.line(screen, (R, G, B), (150, 70), (150, 76), 4)
     pygame.draw.line(screen, (R, G, B), (150, 45), (150, 51), 4)
 
-    # рамка
+    # frame
     pygame.draw.lines(screen, (54, 19, 5), True, [(0, 1), (299, 1), (299, 149), (0, 149)], 1)
 
     pygame.display.flip()
