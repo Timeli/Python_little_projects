190d0929608653068cf7da170e9cde60cb6b2296
Timeli <59762421+Timeli@users.noreply.github.com>
2020-11-03 02:56:27 +0300

    Update top_words.py

--- top_words.py
+++ top_words.py
@@ -1,8 +1,10 @@
 import string
 
+#   get punctuation list
 punctuation = string.punctuation
 
 text_old = open('your text.txt')
+#   convert text to string and lowercase
 text_old = str(text_old.read()).lower()
 
 
@@ -12,13 +14,14 @@ def top_words(text, numb):
         text: lowercase text without punctuation
         numb: number of most popular words
     """
+    #   create a new list replacing punctuation with a space
     text_new = ''
     for i in text:
         if i in punctuation:
             i = ' '
         text_new += i
     text_new = text_new.split()
-
+    #   create a dict where key is word and value is number
     A = {}
     count = 1
     for i in text_new:
@@ -28,7 +31,7 @@ def top_words(text, numb):
             count = 1
         else:
             A[i] = count
-
+    #   get a list of the most popular values in the dict
     top_values = sorted(A.values())[-numb:]
     for key, value in A.items():
         if value in top_values:
